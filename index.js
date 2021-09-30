9bf42f98f5490129b0a17490d7e43e3326e62791
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-01 00:34:12 +0300

    endpoint trycatch startApp

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import Post from './Post.js';
 const PORT = 5000;
+const DB_URL =
+  'mongodb+srv://user:user@cluster0.do71t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json('Сервер работает');
+app.use(express.json());
+
+app.post('/', async (req, res) => {
+  try {
+    const { author, title, content, picture } = req.body;
+    const post = await Post.create({ author, title, content, picture });
+    res.json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-app.listen(PORT, () => {
-  console.log('SERVER STARTED ON PORT' + PORT);
-});
+async function startApp(params) {
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => {
+      console.log('SERVER STARTED ON PORT' + PORT);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+startApp();
