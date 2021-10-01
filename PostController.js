aca3a2fa0d1f35999f22e47becf20dad3c6a9ba8
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-01 18:37:48 +0300

    Service

--- PostController.js
+++ PostController.js
@@ -1,10 +1,10 @@
 import Post from './Post.js';
+import PostService from './PostService.js';
 
 class PostController {
   async create(req, res) {
     try {
-      const { author, title, content, picture } = req.body;
-      const post = await Post.create({ author, title, content, picture });
+      const post = await PostService.create(req.body);
       res.json(post);
     } catch (err) {
       res.status(500).json(err);
@@ -13,7 +13,7 @@ class PostController {
 
   async getAll(req, res) {
     try {
-      const posts = await Post.find();
+      const posts = await PostService.getAll();
       return res.json(posts);
     } catch (err) {
       res.status(500).json(err);
@@ -22,11 +22,7 @@ class PostController {
 
   async getOne(req, res) {
     try {
-      const { id } = req.params;
-      if (!id) {
-        res.status(400).json({ message: 'Id не указан' });
-      }
-      const posts = await Post.findById(id);
+      const posts = await PostService.getOne(req.params.id);
       return res.json(posts);
     } catch (err) {
       res.status(500).json(err);
@@ -35,12 +31,7 @@ class PostController {
 
   async update(req, res) {
     try {
-      const post = req.body;
-      const id = post._id;
-      if (!id) {
-        res.status(400).json({ message: 'Id не указан' });
-      }
-      const updatedPost = await Post.findByIdAndUpdate(id, post, { new: true });
+      const updatedPost = await PostService.update(req.body);
       return res.json(updatedPost);
     } catch (err) {
       res.status(500).json(err);
@@ -49,11 +40,7 @@ class PostController {
 
   async delete(req, res) {
     try {
-      const { id } = req.params;
-      if (!id) {
-        res.status(400).json({ message: 'Id не указан' });
-      }
-      const post = await Post.findByIdAndDelete(id);
+      const post = await PostService.delete(req.params.id);
       return res.json(post);
     } catch (err) {
       res.status(500).json(err);
