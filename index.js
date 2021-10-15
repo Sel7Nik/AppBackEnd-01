6807a779b063a86ae1344beee1ddd75cf64d29c7
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-15 23:29:41 +0300

    try catch

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ const DB_URL =
 app.use(express.json())
 
 //!-- post
+
 app.post('/', async (req, res) => {
-  const { author, title, content, picture } = req.body
-  const post = await Post.create({ author, title, content, picture })
-  res.status(200).json(post)
+  try {
+    const { author, title, content, picture } = req.body
+    const post = await Post.create({ author, title, content, picture })
+    res.status(200).json(post)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
-// //!-- get
-// app.get('/', (req, res) => {
-//   res.status(200).json('Сервер работает')
-// })
 
 async function startApp(params) {
   try {
