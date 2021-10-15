0660a4cb736c535cf9dfd048f2539343dd8d010d
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-15 23:24:25 +0300

    connect MongoDB const Post = new mongoose.Schema адрес подключения к БД await mongoose.connect(DB_URL, const DB_URL = схема таблицы БД

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import Post from './Post.js'
 const PORT = 5000
 
 const app = express()
-
+//!--- адрес подключения к БД
+const DB_URL =
+  'mongodb+srv://user:user@cluster0.do71t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//!--- Если данные были отправлены в формате JSON
 app.use(express.json())
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.status(200).json('Сервер работает')
+//!-- post
+app.post('/', async (req, res) => {
+  const { author, title, content, picture } = req.body
+  const post = await Post.create({ author, title, content, picture })
+  res.status(200).json(post)
 })
+// //!-- get
+// app.get('/', (req, res) => {
+//   res.status(200).json('Сервер работает')
+// })
 
-app.get('/', (req, res) => {
-  res.status(200).json('Сервер работает')
-})
-
-app.listen(PORT, () => {
-  console.log('SERVER STARTED ON PORT' + PORT)
-})
+async function startApp(params) {
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    app.listen(PORT, () => {
+      console.log('SERVER STARTED ON PORT' + PORT)
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+startApp()
