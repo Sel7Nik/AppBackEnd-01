ca86301ce5f0b810365dd06d3d2bdab747fba388
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-17 03:38:37 +0300

    crud  PostController

--- PostController.js
+++ PostController.js
@@ -10,6 +10,55 @@ class PostController {
       res.status(500).json(err)
     }
   }
+
+  async getAll(req, res) {
+    try {
+      const posts = await Post.find()
+      return res.json(posts)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }
+
+  async getOne(req, res) {
+    try {
+      const { id } = req.params
+      if (!id) {
+        res.status(400).json({ message: 'Id не указан' })
+      }
+      const posts = await Post.findById(id)
+      return res.json(posts)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const post = req.body
+      const id = post._id
+      if (!id) {
+        res.status(400).json({ message: 'Id не указан' })
+      }
+      const updatedPost = await Post.findByIdAndUpdate(id, post, { new: true })
+      return res.json(updatedPost)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params
+      if (!id) {
+        res.status(400).json({ message: 'Id не указан' })
+      }
+      const post = await Post.findByIdAndDelete(id)
+      return res.json(post)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }
 }
 
 export default new PostController()
--- router.js
+++ router.js
@@ -5,10 +5,10 @@ const router = new Router()
 //!--- Маршруты для каждой операции
 
 router.post('/posts', PostController.create) //?--- создание
-router.get('/posts') //?--- получение всех
-router.get('/posts/:id') //?--- получение одного по id
-router.put('/posts') //?--- обновление
-router.delete('/posts/:id') //?--- удаление по id
+router.get('/posts', PostController.getAll) //?--- получение всех
+router.get('/posts/:id', PostController.getOne) //?--- получение одного по id
+router.put('/posts', PostController.update) //?--- обновление
+router.delete('/posts/:id', PostController.delete) //?--- удаление по id
 
 //!--- экспортируем роутер
 export default router
